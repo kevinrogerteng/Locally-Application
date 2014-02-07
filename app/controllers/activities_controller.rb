7644f0545c99fc8f1e87932d8e8f8476d47cfdac
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-06 19:33:48 -0800

    fixed MOAR bugs!

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -8,6 +8,7 @@ class ActivitiesController < ApplicationController
 	end
 
 	def create
+		binding.pry
 		session[:return_to] ||= request.referer
 		new_activity = Activity.create(activity_params)
 		validate(new_activity)
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ users << User.create(name: 'Sasha Tang', email_address: "sasha@gmail.com", scree
 
 trips = []
 
-trips << Trip.create(name: 'SF Galore', destination: "San Francisco, California", description:"moving to SF!!")
+trips << Trip.create(name: 'SF Galore', destination: "San Francisco, California", description:"moving to SF!!",)
 trips << Trip.create(name: 'New York Fun!', destination: "Manhattan, New York", description:"Visiting Sasha!")
 trips << Trip.create(name: 'Port Port!',  destination: "Portland, Oregon", description:"Saying hi to Granny!")
 trips << Trip.create(name: 'BBQ IT!', destination: "Dallas, Texas", description:"Let's be a cowboy!")
@@ -18,8 +18,8 @@ users[0].trips << trips[4]
 
 activities =[]
 
-activities << Activity.create(name: 'Metreon', address: '4th and Mission Street, San Francisco, CA', phone: 'N/A', biz_url: "https://www.facebook.com")
-activities << Activity.create(name: 'Westfield', address: '5th and Market Street, San Francisco, CA', phone: 'N/A', biz_url: "https://www.google.com")
+activities << Activity.create(name: 'Metreon', address: '4th and Mission Street, San Francisco, CA', phone: 'N/A', biz_url: "https://www.facebook.com", thumbnail_photo: "http://i.imgur.com/UpJfjiF.gif")
+activities << Activity.create(name: 'Westfield', address: '5th and Market Street, San Francisco, CA', phone: 'N/A', biz_url: "https://www.google.com", thumbnail_photo: "http://i.imgur.com/UpJfjiF.gif")
 
 trips[0].activities << activities[0]
 trips[0].activities << activities[1]
